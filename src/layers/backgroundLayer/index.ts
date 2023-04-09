d1dd2177a0dbe2409584fd67875c17a38db239be
diyigemt <qwe1355247243@gmail.com>
2023-04-09 17:47:35 +0800

    chore: 适配最终版

--- src/layers/backgroundLayer/index.ts
+++ src/layers/backgroundLayer/index.ts
@@ -3,16 +3,20 @@ import * as PIXI from "pixi.js";
 import { Spine } from "pixi-spine";
 import { SoundLayer } from "@/layers/soundLayer";
 let app: Application;
-const KOHARU_ORIGIN_RATE = 3460 / (2568 - 88);
-const KOHARU_PADDING_TOP = 88;
-const KOHARU_PADDING_LEFT = 100;
 const KOHARU_STANDARD_WIDTH = 3460;
 const KOHARU_STANDARD_HEIGHT = 2568;
-const KOHARU_PIVOT_X = -1732.1 + KOHARU_PADDING_LEFT;
+const KOHARU_PADDING_BOTTOM = 88;
+const KOHARU_PADDING_LEFT = 150;
+const KOHARU_ORIGIN_RATE =
+  3460 / (KOHARU_STANDARD_HEIGHT - KOHARU_PADDING_BOTTOM);
+
+const KOHARU_PIVOT_X = -1732.1;
 const KOHARU_PIVOT_Y = -2255.4;
+
 const KOHARU_MOUTH_OFFSET = 1412;
-const KOHARU_BODY_SIZE = 0.38;
-const DLSS = 1;
+const KOHARU_BODY_SIZE = 0.41588;
+const DLSS = 1; // 别动这玩意 你动你自己来调
+
 export const BackgroundLayer = {
   /**
    * 初始化背景
@@ -38,16 +42,7 @@ export const BackgroundLayer = {
       const koharu = new Spine(koharuResource.spineData);
       koharu.state.addAnimation(0, "Idle_01", true, 0);
 
-      SoundLayer.init(
-        koharu.spineData.events
-          .map(event => {
-            if (event.name.startsWith("Talk")) {
-              return "";
-            }
-            return event.name.replace("sound/", "").replace(".wav", "");
-          })
-          .filter(it => it)
-      );
+      SoundLayer.init(koharu);
 
       Reflect.set(koharu, "koharu", "true");
       app.stage.addChild(koharu);
@@ -60,9 +55,9 @@ export const BackgroundLayer = {
       const viewActualWidth = viewStyle.width.computedCssToNumber() / DLSS;
       const offsetX = Math.abs(viewActualWidth - width) / 2;
 
-      (
-        app.view as unknown as HTMLElement
-      ).style.transform = `scale(calc(1 / ${DLSS})) translateX(-${offsetX}px)`;
+      (app.view as unknown as HTMLElement).style.transform = `scale(${
+        1 / DLSS
+      }) translateX(-${offsetX}px)`;
     });
   },
   /**
@@ -86,47 +81,73 @@ export const BackgroundLayer = {
     // 小春的身子距离底部的padding 88px
     // 实际上小春的脸不在正中间 有那么些许偏移150px?
     // 计算逻辑 保证小春的嘴巴位于游戏面板上方50px处?
-    const koharuWidth =
-      koharu.width / koharu.scale.x -
-      (koharu.width / koharu.scale.x / KOHARU_STANDARD_WIDTH) *
-        KOHARU_PADDING_LEFT;
-    const koharuHeight =
-      koharu.height / koharu.scale.y -
-      (koharu.height / koharu.scale.y / KOHARU_STANDARD_HEIGHT) *
-        KOHARU_PADDING_TOP;
+    const koharuWidth = koharu.width / koharu.scale.x;
+    const koharuHeight = koharu.height / koharu.scale.y;
     let finalHeight, finalWidth;
     let scale;
     let mouthOffset = 0;
-    const isVertical = width / height < KOHARU_ORIGIN_RATE;
-    if (isVertical) {
+    let headOffset = 0;
+    if (isMobile()) {
       // 以高度优先
       // 加高保证嘴巴能被看到
-
-      const screenBodyHeight = height * 0.5;
+      const screenBodyHeight = height / 2;
       const sourceBodyHeight = screenBodyHeight / KOHARU_BODY_SIZE;
 
       finalHeight = sourceBodyHeight * DLSS;
 
-      finalHeight = finalHeight + mouthOffset;
       finalWidth = finalHeight * KOHARU_ORIGIN_RATE;
 
       scale = finalHeight / koharuHeight;
-
-      mouthOffset = sourceBodyHeight - screenBodyHeight;
-      console.log(mouthOffset);
+      // headOffset = finalWidth / KOHARU_STANDARD_WIDTH * 150;
+      // headOffset = 0;
+      const mouthY =
+        (finalHeight / KOHARU_STANDARD_HEIGHT) * KOHARU_MOUTH_OFFSET;
+      const screenHeight = height / 2;
+      mouthOffset = (mouthY - screenHeight) / scale - 20;
     } else {
-      // 横屏的时候尽量确保嘴巴在屏幕1/2处
-      finalWidth = width * DLSS;
+      headOffset =
+        (KOHARU_PADDING_LEFT * width) /
+        (KOHARU_STANDARD_WIDTH - KOHARU_PADDING_LEFT);
+      finalWidth = width + headOffset;
       finalHeight = finalWidth / KOHARU_ORIGIN_RATE;
       scale = finalWidth / koharuWidth;
-      mouthOffset =
+
+      const mouthY =
         (finalHeight / KOHARU_STANDARD_HEIGHT) * KOHARU_MOUTH_OFFSET;
-      mouthOffset = mouthOffset - height / 4;
+      const screenHeight = height / 3;
+      mouthOffset = mouthY - screenHeight;
     }
     koharu.scale.set(scale);
-    koharu.pivot.set(KOHARU_PIVOT_X, KOHARU_PIVOT_Y + mouthOffset);
+    koharu.pivot.set(KOHARU_PIVOT_X + headOffset, KOHARU_PIVOT_Y + mouthOffset);
     app.renderer.resize(finalWidth, finalHeight);
   },
 };
 
-export function useApplication() {}
+export function useApplication() {
+  return app;
+}
+
+function isMobile() {
+  let isMobile = false;
+  if ("maxTouchPoints" in navigator) {
+    isMobile = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+    // @ts-ignore
+    isMobile = navigator.msMaxTouchPoints > 0;
+  } else {
+    // @ts-ignore
+    const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+      isMobile = Boolean(mQ.matches);
+    } else if ("orientation" in window) {
+      isMobile = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = navigator.userAgent;
+      isMobile =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+  return isMobile;
+}
--- src/layers/soundLayer/index.ts
+++ src/layers/soundLayer/index.ts
@@ -1,12 +1,21 @@
 import sound from "pixi-sound";
+import { Spine } from "pixi-spine";
 
 export const SoundLayer = {
-  init(soundList: string[]) {
-    soundList.forEach(name => {
-      sound.add(
-        name,
-        `https://yuuka.diyigemt.com/image/ba-all-data/Audio/VoiceJp/Character_voice/JP_Koharu/${name}.mp3`
-      );
-    });
+  init(spine: Spine) {
+    spine.spineData.events
+      .map(event => {
+        if (event.name.startsWith("Talk")) {
+          return "";
+        }
+        return event.name.replace("sound/", "").replace(".wav", "");
+      })
+      .filter(it => it)
+      .forEach(name => {
+        sound.add(
+          name,
+          `https://yuuka.diyigemt.com/image/ba-all-data/Audio/VoiceJp/Character_voice/JP_Koharu/${name}.mp3`
+        );
+      });
   },
 };
